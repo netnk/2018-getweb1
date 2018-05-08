5404d05178b53b01864c655df2438ce5ba28c05c
netnk <netnk0000@gmail.com>
2018-05-08 10:04:24 +0800

    Create json_attr.cs

---
+++ json_attr.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+        public class Stock1
+        {
+            public string stock_name { get; set; }
+            public string stock_price { get; set; }
+
+        }
+   
